87ef3f130a4e629487290e7ce1b7aa0201728662
Laxmi Gurung <samrita.grg20@gmail.com>
2020-05-12 16:24:29 +0545

    departmentListViewController.js imported in the index.html file of the department folder

--- ui/app/department/controllers/departmentListViewController.js
+++ ui/app/department/controllers/departmentListViewController.js
@@ -5,13 +5,14 @@ angular.module('bahmni.department')
         function ($scope, $state, $location, spinner, departmentService) {
             
             var init = function () {
+                console.log("department list view");
                 var params = {};
                 return departmentService.getAllDepartments(params).then(function (response) {
                     $scope.departments = response.data;
                     console.log($scope.departments);
                 });
-                console.log("department list view");
-                return "hi";
+                
+                // return "hi";
             };
 
             return spinner.forPromise(init());
