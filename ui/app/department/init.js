5683b8858854450ed7bcf00e934e2121e70d0d56
Laxmi Gurung <samrita.grg20@gmail.com>
2020-05-12 09:52:20 +0545

    actual code for bahmni

--- ui/app/department/init.js
+++ ui/app/department/init.js
@@ -1,18 +1,18 @@
-// 'use strict';
+'use strict';
 
-// var Bahmni = Bahmni || {};
-// Bahmni.Department = Bahmni.Department || {};
+var Bahmni = Bahmni || {};
+Bahmni.Department = Bahmni.Department || {};
 
-// angular.module('bahmni.department', ['bahmni.common.uiHelper', 'bahmni.common.domain', 'bahmni.common.util', 'bahmni.common.config',
-//     'bahmni.common.orders', 'bahmni.common.appFramework', 'bahmni.common.logging', 'ui.router', 'angularFileUpload']);
+angular.module('bahmni.department', ['bahmni.common.uiHelper', 'bahmni.common.domain', 'bahmni.common.util', 'bahmni.common.config',
+    'bahmni.common.orders', 'bahmni.common.appFramework', 'bahmni.common.logging', 'ui.router']);
 
 
-var app = angular.module('bahmni.department', []);
-    app.controller('PopupDemoController', ['$scope',    function($scope) {
-            $scope.open = function() {
-            	alert("hi");
-                // var modalInstance = $modal.open({
-                //     templateUrl: 'first.html',
-                // });
-            }
-        }]);
+// var app = angular.module('bahmni.department', []);
+// app.controller('PopupDemoController', ['$scope',    function($scope) {
+//     $scope.open = function() {
+//     	alert("hi");
+//         var modalInstance = $modal.open({
+//             templateUrl: 'first.html',
+//         });
+//     }
+// }]);
