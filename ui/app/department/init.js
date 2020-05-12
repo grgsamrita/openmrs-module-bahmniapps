5288ae9894bce8062b423fbd0168e25f0c1a9b09
Laxmi Gurung <samrita.grg20@gmail.com>
2020-05-12 15:39:49 +0545

    ngCookies is required for angular-translate

--- ui/app/department/init.js
+++ ui/app/department/init.js
@@ -3,4 +3,5 @@
 var Bahmni = Bahmni || {};
 Bahmni.Department = Bahmni.Department || {};
 
-angular.module('bahmni.department', ['bahmni.common.uiHelper', 'bahmni.common.util', 'bahmni.common.config','bahmni.common.appFramework', 'bahmni.common.logging', 'ui.router']);
+angular.module('bahmni.department', ['bahmni.common.uiHelper', 'bahmni.common.util', 'bahmni.common.config','bahmni.common.appFramework', 'bahmni.common.logging', 
+	'ui.router', 'ngCookies']);
