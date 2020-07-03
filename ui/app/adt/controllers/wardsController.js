efe4c69277fe19cc946b3c6e06ac5f2c64f9dde4
Laxmi Gurung <samrita.grg20@gmail.com>
2020-07-03 14:08:48 +0545

    console record

--- ui/app/adt/controllers/wardsController.js
+++ ui/app/adt/controllers/wardsController.js
@@ -14,13 +14,13 @@ angular.module('bahmni.adt')
                     $scope.wards = wardsList.results;
 
                     console.log(`laxmi's customisation`);
-                    $scope.wards = _.filter(wardsList.results, function(result) { return (
+                    $scope.wards = _.filter(wardsList.results, function(result) { 
                         console.log(result)
-                        console.log('parent: ' + result.ward.parentLocation)
-                        console.log('visit: '+ $scope.$root.visitLocationUuid)
+                        console.log(result.ward.parentLocation)
+                        console.log($scope.$root.visitLocationUuid)
 
                         // result.ward.parentLocation && result.ward.parentLocation.uuid == $scope.$root.visitLocationUuid
-                        ) });
+                        });
                 });
             };
             spinner.forPromise(init());
